280c4f99622e0fb0f330f5f2ee51198a9280d7ea
arkdru23 <arkadiusz.drukala@gmail.com>
2021-02-12 11:04:41 +0100

    update score and lives

--- js/Game.js
+++ js/Game.js
@@ -5,12 +5,16 @@ class Game {
   #htmlElements = {
     spaceship: document.querySelector("[data-spaceship]"),
     container: document.querySelector("[data-container]"),
+    score: document.querySelector("[data-container]"),
+    lives: document.querySelector("[data-container]"),
   };
   #ship = new Spaceship(
     this.#htmlElements.spaceship,
     this.#htmlElements.container
   );
   #enemies = [];
+  #lives = 0;
+  #score = 0;
   #enemiesInterval = null;
   init() {
     this.#ship.init();
@@ -21,6 +25,8 @@ class Game {
 
   #newGame() {
     this.#enemiesInterval = 30;
+    this.#lives = 3;
+    this.#score = 0;
     this.#createEnemyInterval = setInterval(() => this.#randomNewEnemy(), 1000);
     this.#checkPositionInterval = setInterval(() => this.#checkPosition(), 1);
   }
@@ -31,12 +37,14 @@ class Game {
       ? this.#createNewEnemy(
           this.#htmlElements.container,
           this.#enemiesInterval,
-          "enemy"
+          "enemy",
+          "explosion"
         )
       : this.#createNewEnemy(
           this.#htmlElements.container,
           this.#enemiesInterval * 2,
           "enemy--big",
+          "explosion--big",
           3
         );
   }
@@ -48,20 +56,6 @@ class Game {
   }
 
   #checkPosition() {
-    this.#ship.missiles.forEach((missile, missileIndex, missileArray) => {
-      const missilePosition = {
-        top: missile.element.offsetTop,
-        right: missile.element.offsetLeft + missile.element.offsetWidth,
-        bottom: missile.element.offsetTop + missile.element.offsetHeight,
-        left: missile.element.offsetLeft,
-      };
-
-      if (missilePosition.bottom < 0) {
-        missile.remove();
-        missileArray.splice(missileIndex, 1);
-      }
-    });
-
     this.#enemies.forEach((enemy, enemyIndex, enemyArr) => {
       const enemyPosition = {
         top: enemy.element.offsetTop,
@@ -71,10 +65,64 @@ class Game {
       };
 
       if (enemyPosition.top > window.innerHeight) {
-        enemy.remove();
+        enemy.explode();
         enemyArr.splice(enemyIndex, 1);
       }
+
+      this.#ship.missiles.forEach((missile, missileIndex, missileArr) => {
+        const missilePosition = {
+          top: missile.element.offsetTop,
+          right: missile.element.offsetLeft + missile.element.offsetWidth,
+          bottom: missile.element.offsetTop + missile.element.offsetHeight,
+          left: missile.element.offsetLeft,
+        };
+
+        if (
+          missilePosition.bottom >= enemyPosition.top &&
+          missilePosition.top <= enemyPosition.bottom &&
+          missilePosition.right >= enemyPosition.left &&
+          missilePosition.left <= enemyPosition.right
+        ) {
+          enemy.hit();
+          if (!enemy.lives) {
+            enemyArr.splice(enemyIndex, 1);
+          }
+          missile.remove();
+          missileArr.splice(missileIndex, 1);
+          this.#updateScore();
+        }
+
+        if (missilePosition.bottom < 0) {
+          missile.remove();
+          missileArr.splice(missileIndex, 1);
+          this.#updateLives();
+        }
+      });
     });
+  }
+
+  #updateScore() {
+    this.#score++;
+
+    if (!this.#score % 5) {
+      this.#enemiesInterval--;
+    }
+
+    this.#updateScoreText();
+  }
+
+  #updateLives() {
+    this.#lives--;
+    this.#updateLivesText();
+    this.#htmlElements.container.classList.add("hit");
+    setTimeout(() => this.#htmlElements.container.classList.remove("hit"), 100);
+  }
+
+  #updateScoreText() {
+    this.#htmlElements.score.textContent = `Score: ${this.#score}`;
+  }
+  #updateLivesText() {
+    this.#htmlElements.lives.textContent = `Lives: ${this.#lives}`;
   }
 }
 
